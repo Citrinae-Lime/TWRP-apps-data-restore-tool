9deb78dcf1af42c5ff1379438744657272e07aa1
Citrinae-Lime <Citrinae-Lime@users.noreply.github.com>
2024-10-19 23:44:49 +0200

    Add NoReinstall option
    And more tips

--- 2_RestorePackages.ps1
+++ 2_RestorePackages.ps1
@@ -15,11 +15,14 @@
   .PARAMETER LocalApkPath
   Path to local data\app folder
 
+  .PARAMETER NoReinstall
+  If specified, the script will not reinstall the APKs of the packages.
+
   .EXAMPLE
   .\2_RestorePackages.ps1 $(Get-Content Packages.txt) -lpkg E:\Path\to\TWRP\BACKUPS\*\data\data\ -lapk E:\Path\to\TWRP\BACKUPS\*\data\app\
 
   .EXAMPLE
-  .\2_RestorePackages.ps1 "com.test.app","com.other.app"
+  .\2_RestorePackages.ps1 "com.test.app","com.other.app" -NoReinstall
 #>
 
 [CmdletBinding()]
@@ -27,7 +30,8 @@ Param (
     [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][object]$pkgList,
     # TWRP extract location, change if necessary!
     [Parameter(Mandatory=$False)][ValidateNotNullOrEmpty()][Alias("lpkg")][String]$LocalPackages = ".\data\data\",
-    [Parameter(Mandatory=$False)][ValidateNotNullOrEmpty()][Alias("lapk")][String]$LocalApkPath = ".\data\app\"
+    [Parameter(Mandatory=$False)][ValidateNotNullOrEmpty()][Alias("lapk")][String]$LocalApkPath = ".\data\app\",
+    [Parameter(Mandatory=$False)][Switch]$NoReinstall
 )
 
 # Android delivery destination
@@ -41,14 +45,14 @@ if ($LASTEXITCODE = 1) {
     Write-Host "Could not find adb.exe"
     exit $LASTEXITCODE
 }
-# .\adb.exe kill-server
-# .\adb.exe start-server
+.\adb.exe kill-server
+.\adb.exe start-server
 Write-Host "Checking ADB as root"
 .\adb.exe root
 if ($(.\adb.exe shell id -u) -ne 0) {
     if ($(.\adb.exe shell su -c whoami) -eq "root") {$libsu="su -c"}
     else {
-        Write-Host "[ERROR] Didn't get root permissions! Can't restore." -ForegroundColor Red
+        Write-Host "Didn't get root permissions! Can't restore." -ForegroundColor Red
         Write-Host "You can chose one of the following two ways to be able to restore:"
         Write-Host " - Install a modded adbd version on your device"
         Write-Host " - Root your device and allow root access for 'Shell' package"
@@ -58,53 +62,48 @@ function AdbShell {param([Parameter(Mandatory=$true)][string]$Command).\adb.exe 
 # Write-Host "========================================================="
 
 function Restore-Package {
-    param (
-        [Parameter(Mandatory=$true)][string]$package
-    )
+    param ([Parameter(Mandatory=$true)][string]$package)
     Write-Host "========================================================="
 
     AdbShell "pm path $package"
     $exists = $LASTEXITCODE
 
-    # Userinstalled Apps
     if ($exists -eq 0) {
         Write-Host "Killing $package"
         AdbShell "am force-stop $package"
         Write-Host "Clearing $package"
         AdbShell "pm clear $package"
-    }
-
-    Write-Host "Reinstalling apk of $package"
-    .\adb.exe install-multiple -r $(Get-ChildItem -Path "$LocalApkPath\*\$package-*\*.apk").FullName
-    if ($LASTEXITCODE -ne 0) {
-        Write-Host "Failed to install $package" -ForegroundColor Red
+    } elseif ($NoReinstall) {
+        Write-Host "Failed to find preinstalled $package" -ForegroundColor Red
         return
     }
 
-    # Preinstalled Apps
-    # if ($exists -ne 0) {
-    #     Write-Host "Failed to find preinstalled $package" -ForegroundColor DarkYellow
-    #     return
-    # }
-    # Write-Host "Killing $package"
-    # AdbShell "am force-stop $package"
-    # Write-Host "Clearing $package"
-    # AdbShell "pm clear $package"
-    
+    if (-not $NoReinstall) {
+        Write-Host "Reinstalling apk of $package"
+        .\adb.exe install-multiple -r $(Get-ChildItem -Path "$LocalApkPath\*\$package-*\*.apk").FullName
+        if ($LASTEXITCODE -ne 0) {
+            Write-Host "Failed to install $package" -ForegroundColor Red
+            return
+        }
+    }
+
     # Remove cache and compress to save tame
+    Write-Host "Compressing $package"
     # Remove-Item -Path "$LocalPackages$package\cache" -Recurse
     Remove-Item -Path "$LocalPackages$package\code_cache" -Recurse
     tar.exe -czf "$package.tgz" --exclude "cache" -C $LocalPackages $package
 
-    Write-Host "Restoring $package"
+    Write-Host "Copying $package"
     .\adb.exe push "$package.tgz" "$TempPackages/$package.tgz"
     # $userid=AdbShell "stat -c '%U' $RemotePackages$package"
     # $groupid=AdbShell "stat -c '%G' $RemotePackages$package"
+    $userid = AdbShell "dumpsys package $package" | Select-String "userId" | ForEach-Object { $_.Line.Split('=')[1].Trim() }
+
+    Write-Host "Restoring $package"
     AdbShell "tar xfz $TempPackages$package.tgz -C $RemotePackages && rm $TempPackages$package.tgz"
     # AdbShell cp -r "$TempPackages$package" "$RemotePackages"
 
     Write-Host "Correcting package"
-    $userid = AdbShell "dumpsys package $package" | Select-String "userId" | ForEach-Object { $_.Line.Split('=')[1].Trim() }
     AdbShell chown -R "$userid`:$userid" "$RemotePackages$package"
     # AdbShell "chown -R $userid`:$groupid $RemotePackages$package"
     AdbShell "restorecon -Rv $RemotePackages$package"
